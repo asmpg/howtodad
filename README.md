1ed70d527c91b0ebfc5df3f59cd43b91eddda7c0
ChenWencong <wencong.chen@outlook.com>
2022-01-26 15:21:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+## 参考
+
+- https://vuepress.vuejs.org/theme/default-theme-config.html#navbar-links
+- https://vitepress.vuejs.org/guide/deploy.html
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	title: 'How to Dad',
+	description: '这是一本关于如何成为一个爸爸的书籍',
+	base: '/',
+	lang: 'zh-CN',
+	themeConfig: {
+		nav: [{
+				text: 'Home',
+				link: '/'
+			}, {
+				text: '不得不做',
+				link: '/mustdo/'
+			}, {
+				text: '行为分析',
+				link: '/actions/'
+			},
+			{
+				text: 'External',
+				link: 'https://google.com'
+			}
+		]
+	}
+
+}
